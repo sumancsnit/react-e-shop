ec123faa7c3cd240ae480cb2e2b0f62d96a9ffe2
Suman Kumar <sumancsnit@gmail.com>
2020-06-13 23:29:40 +0530

    initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
