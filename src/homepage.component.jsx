60c76948da9a3b2b96cfb427caef64477e08b4a6
Suman Kumar <sumancsnit@gmail.com>
2020-06-14 22:07:27 +0530

    scss added

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,36 +1,41 @@
 import React from 'react';
 
+import './homepage.style.scss';
+
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>HATS</h1>
-          <span className='subtitle'>SHOP NOW</span>
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>HATS</h1>
-          <span className='subtitle'>SHOP NOW</span>
+
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>HATS</h1>
-          <span className='subtitle'>SHOP NOW</span>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>HATS</h1>
-          <span className='subtitle'>SHOP NOW</span>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
         </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>HATS</h1>
-          <span className='subtitle'>SHOP NOW</span>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
         </div>
       </div>
     </div>
