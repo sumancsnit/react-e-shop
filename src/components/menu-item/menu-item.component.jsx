d39cd8c1d956afeb92581363e82f625fd3812a57
Suman Kumar <sumancsnit@gmail.com>
2020-06-15 19:52:35 +0530

    split components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
-  console.log('MenuItem -> imageURL', imageUrl);
-
-  return (
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
     <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`${size} menu-item`}
-    >
-      <div className='content'>
-        <h1 className='title'>{title}</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-  );
-};
+  </div>
+);
 
 export default MenuItem;
