59c6cf602254c1aec742b4175e62acc3f929b394
Suman Kumar <sumancsnit@gmail.com>
2020-06-15 19:32:34 +0530

    added menue item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './homepage.style.scss';
+// other component
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.style.scss';
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
