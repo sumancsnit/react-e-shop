4ae3dd1e286ec009c2023e1120f0fc7ae7805196
Suman Kumar <sumancsnit@gmail.com>
2020-07-02 16:55:33 +0530

    cart-icon added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 import './header.styles.scss';
 
 import { auth } from '../../firebase/firebase.utils';
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -28,6 +28,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
