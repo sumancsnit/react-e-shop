38af9b73a9f025e59de71cb4edb6f317bd3478a6
Suman Kumar <sumancsnit@gmail.com>
2020-07-03 23:08:58 +0530

    implemented reselect lib, replaced cart selectors with memoized selectors

---
+++ src/redux/cart/cart.selector.js
@@ -0,0 +1,22 @@
+import { createSelector } from 'reselect';
+
+// input selector, it selects from state
+const selectCart = (state) => state.cart;
+
+// output selector using createSelector from reselect lib, it will check if any change occur in state.cart
+
+export const selectCartItems = createSelector(
+  [selectCart], // collection of input selector, it can be multiple
+  // selectCart as cart
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  //selectCartItems as cartItems
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulator, cartItem) => accumulator + cartItem.quantity,
+      0
+    )
+);
