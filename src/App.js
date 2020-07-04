e8612843c3ee04fc11a7349ecb82e2cc77f2b06c
Suman Kumar <sumancsnit@gmail.com>
2020-07-04 22:30:11 +0530

    data nomalization and router updated

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
-          <Route exact path='/shop' component={ShopPage} />
+          <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
 
           <Route
