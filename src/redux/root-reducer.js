e547519f202cd68aa66118e15ed906785467e5ca
Suman Kumar <sumancsnit@gmail.com>
2020-07-04 16:07:32 +0530

    redux - persist implemented

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,23 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+//use localstorage persist
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root', // from root level
+  storage, // type of storage
+  whitelist: ['cart'], // persist cart data onlint inside state
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+// modified version of root reducer
+export default persistReducer(persistConfig, rootReducer);
+
+// persist 2
