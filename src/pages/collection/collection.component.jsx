0218496ac93ca1a8d1ae70d88386b0f1d1ed9219
Suman Kumar <sumancsnit@gmail.com>
2020-07-06 16:29:33 +0530

    with spinner HOC created

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,8 +7,8 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
-  console.log('CollectionPage -> collection', collection);
+// todo
+const CollectionPage = ({ collection = {} }) => {
   const { title, items } = collection;
   return (
     <div className='collection-page'>
